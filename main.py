f3e72a697606beaa8c47c4770f75542b917a9a3c
Frank <h0930117602@gmail.com>
2025-04-21 01:02:55 +0800

    +) 修改中文 doc

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.messenger.webhook import router as webhook_router
 
 app = FastAPI(
-    title="劉宇軒的後端Messenger Webhook",
+    title="Messenger Webhook",
     docs_url="/docs")
 
 app.include_router(webhook_router)
